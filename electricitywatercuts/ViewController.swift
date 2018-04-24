49981d6ea409f63429f2efd81f0645ec72639ed6
nils <recya@localhost>
2018-04-24 17:49:53 +0300

    Initial Commit

---
+++ electricitywatercuts/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  electricitywatercuts
+//
+//  Created by nils on 24.04.2018.
+//  Copyright © 2018 nils. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
